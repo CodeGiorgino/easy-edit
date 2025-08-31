838789b48465d7d7f07e061b176b0fd991f3f764
CodeGiorgino <andreagiorgino1@gmail.com>
2025-08-31 03:44:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Notes
+Simple text editor written in c++ using [raylib](https://github.com/raysan5/raylib)
---
+++ src/main.cc
@@ -0,0 +1,20 @@
+#include "raylib.h"
+
+int main(void)
+{
+    InitWindow(800, 450, "notes");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
